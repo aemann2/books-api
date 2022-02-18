6541fca0007fea67686d89349086d2a4791d0944
Adam Mann <68879246+aemann2@users.noreply.github.com>
2022-02-18 10:38:09 -0800

    setup/mongoDB (#11)
    * added express boilerplate and types
    
    * added TS boilerplate and dependencies for backend
    
    * Setup/mongo db (#9)
    
    * added .env.sample
    
    * added cors and dotenv packages
    
    * added mongoDB connection config

--- server.ts
+++ server.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from 'express';
 
+const cors = require('cors');
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/.env' });
+const db = require('./config/db');
+
 const app = express();
-const port = 5000;
+app.use(cors());
+const PORT: String | Number = process.env.PORT || 5000;
+
+db();
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello world!');
 });
 
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
 });
