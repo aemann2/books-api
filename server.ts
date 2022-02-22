fd3dda8434702501444094b28b2d18a54a978764
aemann2 <aemann2@gmail.com>
2022-02-22 13:23:33 -0800

    added static path in server.ts

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/.env' });
 const db = require('./config/db');
@@ -11,6 +12,12 @@ app.use(cors());
 const PORT: String | Number = process.env.PORT || 5000;
 
 db();
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+app.get('*', function (request: Request, response: Response) {
+	response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello world!');
